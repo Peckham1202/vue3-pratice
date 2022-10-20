7fc36df5e494e3b4708aab0ebfd4aba21a800bdb
无情月 <pichamli@163.com>
2022-10-20 23:22:18 +0800

    添加单文件和非单文件组件示例

---
+++ vue/vue_basic/18_单文件组件/test1/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <School></School>
+    <Student></Student>
+  </div>
+</template>
+
+<script>
+// import { defineComponent } from '@vue/composition-api'
+import Student from './components/Student.vue';
+import School from './components/School.vue';
+
+export default {
+  name: 'App',
+  components: {
+    School,
+    Student
+  }
+}
+</script>
---
+++ vue/vue_basic/18_单文件组件/test1/src/components/School.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="school">
+    <h2>学校名称：{{name}}</h2>
+    <h2>学校地址：{{address}}</h2>
+    <button @click="showName">点我提示学校名称</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'School',
+  data() {
+    return {
+      name: '牛肉丸大学',
+      address: '广东广州'
+    }
+  },
+  methods: {
+    showName() {
+      alert(this.name);
+    }
+  }
+}
+</script>
+
+<style>
+.school {
+  background-color: orange;
+}
+</style>
