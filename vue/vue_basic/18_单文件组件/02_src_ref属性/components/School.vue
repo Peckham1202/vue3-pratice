ad862aa55fe0f3cb073c4ada636bc4f8bdaccf3f
无情月 <pichamli@163.com>
2022-10-24 23:46:29 +0800

    整理 ref属性 示例 添加 props配置 示例

--- vue/vue_basic/18_单文件组件/02_src_ref属性/components/School.vue
+++ vue/vue_basic/18_单文件组件/02_src_ref属性/components/School.vue
@@ -2,7 +2,6 @@
   <div class="school">
     <h2>学校名称：{{name}}</h2>
     <h2>学校地址：{{address}}</h2>
-    <button @click="showName">点我提示学校名称</button>
   </div>
 </template>
 
@@ -12,19 +11,14 @@ export default {
   data() {
     return {
       name: '牛肉丸大学',
-      address: '广东广州'
-    }
+      address: '广东·广州'
+    };
   },
-  methods: {
-    showName() {
-      alert(this.name);
-    }
-  }
 }
 </script>
 
 <style>
 .school {
-  background-color: orange;
+  background-color: gray;
 }
 </style>
