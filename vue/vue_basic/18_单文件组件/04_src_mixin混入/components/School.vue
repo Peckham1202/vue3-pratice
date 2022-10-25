bc65485708f64999b7a1ec5b1f986c1bc766a015
无情月 <pichamli@163.com>
2022-10-25 22:54:02 +0800

    添加 mixin混入 示例

---
+++ vue/vue_basic/18_单文件组件/04_src_mixin混入/components/School.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2 @click="showName">学校名称：{{ name }}</h2>
+    <h2>学校地址：{{ address }}</h2>
+  </div>
+</template>
+
+<script>
+import { hunhe, hunhe2 } from '@/mixin';
+
+export default {
+  name: 'School',
+  data() {
+    return {
+      name: 'BiliBili',
+      address: '广州',
+      x: 666,
+    };
+  },
+  mixins: [hunhe, hunhe2]
+}
+</script>
