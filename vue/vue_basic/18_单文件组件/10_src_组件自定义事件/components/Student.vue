af6a59f8dbe824dc287e9980443ed6074aa9d130
无情月 <pichamli@163.com>
2022-11-02 23:38:39 +0800

    添加 组件自定义事件 示例

---
+++ vue/vue_basic/18_单文件组件/10_src_组件自定义事件/components/Student.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="student">
+    <h2>学生姓名：{{ name }}</h2>
+    <h2>学生性别：{{ sex }}</h2>
+    <h2>当前求和为：{{ number }}</h2>
+    <button @click="add">点我number++</button>
+    <button @click="sendStudentName">把学生名给App</button>
+    <button @click="unbind">解绑onname事件</button>
+    <button @click="death">销毁当前Student组件实例</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Student',
+  data() {
+    return {
+      name: '张三',
+      sex: '男',
+      number: 0
+    }
+  },
+  methods: {
+    add() {
+      console.log('add回调被调用了');
+      this.number++;
+    },
+    sendStudentName() {
+      this.$emit('onname', this.name, 666, 888, 900);
+    },
+    unbind() {
+      this.$off('onname');
+    },
+    death() {
+      this.$destroy();
+    }
+  }
+}
+</script>
+
+<style>
+.student {
+  background-color: pink;
+  padding: 5px;
+  margin-top: 30px;
+}
+</style>
