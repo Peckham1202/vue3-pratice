6a51e75d436995c8e7226ee3cb1b4fff301c46a0
无情月 <pichamli@163.com>
2022-11-08 23:06:12 +0800

    添加 过渡与动画 示例

---
+++ vue/vue_basic/18_单文件组件/17_src_过度与动画/components/Test2.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <button @click="isShow = !isShow">显示/隐藏</button>
+    <transition-group name="hello" appear>
+      <h1 v-show="isShow" key="1">你好啊！</h1>
+      <h1 v-show="!isShow" key="2">牛肉丸</h1>
+    </transition-group>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test2',
+  data() {
+    return {
+      isShow: true
+    }
+  }
+}
+</script>
+
+<style>
+h1 {
+  background-color: pink;
+}
+
+.hello-enter,
+.hello-leave-to {
+  transform: translateX(-100%);
+}
+
+.hello-enter-to,
+.hello-leave {
+  transform: translateX(0)
+}
+
+.hello-enter-active,
+.hello-leave-active {
+  transition: 0.5s linear;
+}
+</style>
