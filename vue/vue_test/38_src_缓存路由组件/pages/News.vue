600136e4287796566b56ed52a0cf0e0a6770fc54
无情月 <pichamli@163.com>
2023-02-27 23:50:05 +0800

    添加 缓存路由组件 示例

---
+++ vue/vue_test/38_src_缓存路由组件/pages/News.vue
@@ -0,0 +1,13 @@
+<template>
+  <ul>
+    <li>news001 <input></li>
+    <li>news002 <input></li>
+    <li>news003 <input></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'News'
+}
+</script>
