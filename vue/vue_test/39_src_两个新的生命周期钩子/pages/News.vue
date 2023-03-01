630fb4debed92d79dd5924b95c298b27920326a2
无情月 <pichamli@163.com>
2023-03-01 22:51:51 +0800

    添加 两个新的生命周期钩子 的示例

---
+++ vue/vue_test/39_src_两个新的生命周期钩子/pages/News.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul>
+    <li :style="{ opacity }">我来学习Vue了！！！</li>
+    <li>news001 <input></li>
+    <li>news002 <input></li>
+    <li>news003 <input></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'News',
+  data() {
+    return {
+      opacity: 1,
+    };
+  },
+
+  activated() {
+    this.timer = setInterval(() => {
+      console.log('@');
+      this.opacity -= 0.01;
+      if (this.opacity < 0) this.opacity = 1;
+    }, 16);
+  },
+
+  deactivated() {
+    clearInterval(this.timer);
+  }
+}
+</script>
