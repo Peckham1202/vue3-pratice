06454a88d9593065d91a6f95f09bb427b8b017f1
无情月 <pichamli@163.com>
2023-03-20 23:47:44 +0800

    添加 全局路由守卫 示例

---
+++ vue/vue_test/40_src_全局路由守卫/pages/News.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul>
+    <li :style="{ opacity }">我来学习Vue了！！！</li>
+    <li>news001 <input></li>
+    <li>news002 <input></li>
+    <li>news003 <input></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'News',
+  data() {
+    return {
+      opacity: 1,
+    };
+  },
+
+  activated() {
+    console.log('News组件被激活了');
+    this.timer = setInterval(() => {
+      console.log('@');
+      this.opacity -= 0.01;
+      if (this.opacity < 0) this.opacity = 1;
+    }, 16);
+  },
+
+  deactivated() {
+    console.log('News组件失活了');
+    clearInterval(this.timer);
+  }
+}
+</script>
