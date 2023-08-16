3c123809a8d662f34ef5f4f952543520f5b4da86
无情月 <pichamli@163.com>
2023-08-16 23:20:23 +0800

    [vue3] 添加 setup 示例

---
+++ vue/vue3_test/02_src_初识setup/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>一个人的信息</h1>
+  <h2>姓名：{{ name }}</h2>
+  <h2>年龄：{{ age }}</h2>
+  <h2>性别：{{ sex }}</h2>
+  <button @click="sayHello">欢迎光临</button>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  setup() {
+    let name = '张三';
+    let age = 18;
+    let sex = '男';
+
+    const sayHello = () => {
+      alert(`我叫${name}，我${age}岁了， 你好啊！`);
+    };
+
+    return {
+      name,
+      age,
+      sex,
+      sayHello
+    }
+  }
+}
+</script>
