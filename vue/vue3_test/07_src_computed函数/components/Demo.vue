bd166d48409d691ae9d128290c4760776c4cf22f
无情月 <pichamli@163.com>
2023-08-21 22:32:11 +0800

    [vue3] 添加 computed函数 示例

---
+++ vue/vue3_test/07_src_computed函数/components/Demo.vue
@@ -0,0 +1,40 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <h1>一个人的信息</h1>
+  姓：<input type="text" v-model="person.firstName" />
+  <br />
+  名：<input type="text" v-model="person.lastName" />
+  <br />
+  全名：<input type="text" v-model="person.fullName" />
+</template>
+
+<script>
+import { reactive, computed } from 'vue';
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Demo',
+  setup() {
+    let person = reactive({
+      firstName: '张',
+      lastName: '三'
+    });
+
+    person.fullName = computed({
+      get() {
+        return person.firstName + '-' + person.lastName;
+      },
+
+      set(value) {
+        const nameArr = value.split('-');
+        person.firstName = nameArr[0];
+        person.lastName = nameArr[1];
+      }
+    })
+
+    return {
+      person
+    }
+  },
+}
+</script>
