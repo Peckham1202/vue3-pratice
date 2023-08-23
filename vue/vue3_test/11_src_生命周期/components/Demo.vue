16076c33ae4da18adfb792a8f6ceb97afba1db70
无情月 <pichamli@163.com>
2023-08-23 21:03:25 +0800

    添加 生命周期 示例

---
+++ vue/vue3_test/11_src_生命周期/components/Demo.vue
@@ -0,0 +1,80 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <h2>当前求和为：{{ sum }}</h2>
+  <button @click="sum++">点我+1</button>
+</template>
+
+<script>
+import { onBeforeMount, onBeforeUnmount, onBeforeUpdate, onMounted, onUnmounted, onUpdated, ref } from 'vue';
+
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Demo',
+  setup() {
+    console.log('---setup---');
+
+    let sum = ref(0);
+
+    //通过组合式API的形式去使用生命周期钩子
+    onBeforeMount(() => {
+      console.log('---onBeforeMount---');
+    });
+
+    onMounted(() => {
+      console.log('---onMounted---');
+    });
+
+    onBeforeUpdate(() => {
+      console.log('---onBeforeUpdate---');
+    });
+
+    onUpdated(() => {
+      console.log('---onUpdated---');
+    });
+
+    onBeforeUnmount(() => {
+      console.log('---onBeforeUnmount---');
+    });
+
+    onUnmounted(() => {
+      console.log('---onUnmounted---');
+    });
+
+    return { sum };
+  },
+
+  //通过配置项的形式使用生命周期钩子
+  beforeCreate() {
+    console.log('---beforeCreate---');
+  },
+
+  created() {
+    console.log('---created---');
+  },
+
+  beforeMount() {
+    console.log('---beforeMount---');
+  },
+
+  mounted() {
+    console.log('---mounted---');
+  },
+
+  beforeUpdate() {
+    console.log('---beforeUpdate---');
+  },
+
+  updated() {
+    console.log('---updated---');
+  },
+
+  beforeUnmount() {
+    console.log('---beforeUnmount---');
+  },
+
+  unmounted() {
+    console.log('---unmounted---');
+  }
+}
+</script>
