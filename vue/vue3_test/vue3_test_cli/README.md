469d0f8bf95abdd5fbab8160aa4f1eb94eeb9603
无情月 <pichamli@163.com>
2023-08-24 21:17:15 +0800

    [vue3] 添加 模板工程： 将 示例代码 替换掉 src，即可执行对应的示例。

---
+++ vue/vue3_test/vue3_test_cli/README.md
@@ -0,0 +1,3 @@
+# vue3_test_cli
+
+用 vue-cli 生成出来的模板工程，将外部的示例 src 目录替换掉当前的 src 目录，再执行 npm run serve 即可执行相应的示例。
---
+++ vue/vue3_test/vue3_test_cli/src/components/Demo.vue
@@ -0,0 +1,27 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <h2>当前求和为：{{ sum }}</h2>
+  <button @click="sum++">点我+1</button>
+  <hr />
+  <h2>当前点击时鼠标的坐标为：x: {{ point.x }}, y: {{ point.y }}</h2>
+</template>
+
+<script>
+import usePoint from '@/hooks/usePoint';
+import { ref } from 'vue';
+
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Demo',
+  setup() {
+    console.log('---setup---');
+
+    let sum = ref(0);
+    let point = usePoint();
+
+
+    return { sum, point };
+  }
+}
+</script>
